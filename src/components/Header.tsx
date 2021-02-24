9034c800aeb4d80bed1784e6ab455d849f559bca
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-02-24 23:33:11 +0900

    headerのスタイル

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {Heading, Text, GridItem, Grid, Button} from '@chakra-ui/react'
 
 const Header: React.FC = () => {
   return (
     <>
-      <Link to="/">トップに戻る</Link>
-      <h1>Search-NPB</h1>
-      <p>NPBの背番号の歴史を調べることができるサイトです</p>
+      <Grid templateColumns="repeat(6, 1fr)">
+        <GridItem colSpan={5}>
+          <Link to="/"><Heading>Search-NPB</Heading></Link>
+          <Text>NPBの背番号の歴史を調べることができるサイトです</Text>
+        </GridItem>
+        <GridItem colSpan={1} style={{textAlign: 'right'}}>
+          <Button>マイページ</Button>
+        </GridItem>
+      </Grid>
     </>
   );
 };
