7047193e0c0ac0335f9a864304fb1ee704351132
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-02-27 13:58:29 +0900

    ログイン状態を担保

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useRecoilState } from "recoil";
 import Main from "./components/Main";
 import Header from "./components/Header";
 import SearchTeam from "./components/Team/Search";
@@ -9,6 +10,8 @@ import styled from "styled-components";
 import SearchNumber from "./components/Number/Search";
 import ResultsNumber from "./components/Number/Results";
 import Footer from "./components/Footer";
+import { userState } from "./store/main";
+import firebase from "./lib/firebase";
 
 const Div = styled.div`
   width: 98%;
@@ -17,23 +20,47 @@ const Div = styled.div`
 `;
 
 const App: React.FC = () => {
-  return (
-    <div style={{ padding: "30px" }}>
-      <Router>
-        <Header />
-        <Div />
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route exact path="/search/team" component={SearchTeam} />
-          <Route exact path="/search/number" component={SearchNumber} />
-          <Route exact path="/results/team" component={ResultsTeam} />
-          <Route exact path="/results/number" component={ResultsNumber} />
-          <Route exact path="/results/keyword" component={ResultsKeyword} />
-        </Switch>
-        <Footer />
-      </Router>
-    </div>
-  );
+  const [user, setUser] = useRecoilState(userState);
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setLoading(true);
+    console.log("effect");
+    if (user.uid === "") {
+      console.log("start");
+      firebase.auth().onAuthStateChanged(async (u) => {
+        console.log(u);
+        await setUser({
+          uid: u?.uid,
+          email: u?.email,
+          name: u?.displayName,
+        });
+        setLoading(false);
+      });
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  if (loading) {
+    return <div />;
+  } else {
+    return (
+      <div style={{ padding: "30px" }}>
+        <Router>
+          <Header />
+          <Div />
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/search/team" component={SearchTeam} />
+            <Route exact path="/search/number" component={SearchNumber} />
+            <Route exact path="/results/team" component={ResultsTeam} />
+            <Route exact path="/results/number" component={ResultsNumber} />
+            <Route exact path="/results/keyword" component={ResultsKeyword} />
+          </Switch>
+          <Footer />
+        </Router>
+      </div>
+    );
+  }
 };
 
 export default App;
