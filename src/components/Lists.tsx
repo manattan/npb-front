237f46b68831d1b59d42f0c2b7e0d0f8233f7594
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-03-06 22:55:52 +0900

    ログイン時のみ編集できる

--- src/components/Lists.tsx
+++ src/components/Lists.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
+import { useRecoilValue } from "recoil";
 import * as types from "../types/main";
 import { Li } from "../components/StyledComponent/index";
 import { getTeam } from "../lib/main";
+import { userState } from "../store/main"
 
 interface Props {
   lists: types.Data[];
 }
 
 const Lists: React.FC<Props> = (props: Props) => {
+  const user = useRecoilValue(userState)
   const renderList: Array<JSX.Element> = [];
   for (const tmp of props.lists) {
     renderList.push(
       <Li key={tmp.id}>
-        <Button style={{ float: "right" }}>編集する</Button>
+        {user && (
+          <Button style={{ float: "right" }}>編集する</Button>
+        )}
         <p>{getTeam(tmp.teamname)}</p>
         <p>背番号: {tmp.num}</p>
         <p>{tmp.history}</p>
