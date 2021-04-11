84b9c258ac196330866b0fa782a564218b62146f
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-04-11 21:18:45 +0900

    header をいい感じにする

---
+++ src/components/StyledComponent/header.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import {sp, pc} from './meta'
+
+export const HeaderContainer = styled.div`
+  height: 60px;
+  width: 100vw;
+  background-color: #ffbb33;
+  position: sticky;
+  top: 0px;
+  z-index: 10;
+  justify-content: space-around;
+  display: flex;
+`
+
+export const LeftContainer = styled.div`
+  display: flex;
+  margin: 6px 0;
+`
+
+export const LeftObj = styled.div`
+  display: flex;
+  h1 {
+    font-weight: bold;
+    font-size: 2rem;
+    color: white;
+    cursor: pointer;
+    user-select: none;
+  } 
+`
+
+export const RightContainer = styled.div`
+  display: flex;
+  ${sp`
+    display: none;
+  `}
+`
+
+export const RightObj = styled.div`
+  display: flex;
+  p {
+    margin: 18px;
+  }
+  button{
+    margin: 10px 10px 0;
+  }
+`
+
+export const HamburgerContainer = styled.div`
+  margin-top: 10px;
+  ${pc`
+    display: none;
+  `}
+`
+
